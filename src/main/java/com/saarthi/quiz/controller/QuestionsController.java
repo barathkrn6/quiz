8fdfa994ee8e5fa25855180deb14a22b7d5184bf
Barath Karunakaran <barath@saarthiapp.com>
2022-01-26 18:49:07 +0530

    api

--- src/main/java/com/saarthi/quiz/controller/QuestionsController.java
+++ src/main/java/com/saarthi/quiz/controller/QuestionsController.java
@@ -1,6 +1,8 @@
 package com.saarthi.quiz.controller;
 
 import com.saarthi.quiz.model.db.Questions;
+import com.saarthi.quiz.model.db.QuizSchedule;
+import com.saarthi.quiz.repo.QuizScheduleRepository;
 import com.saarthi.quiz.service.QuestionsService;
 import com.saarthi.quiz.service.impl.AsyncServiceImpl;
 import org.slf4j.Logger;
@@ -9,6 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
@@ -27,6 +32,9 @@ public class QuestionsController {
 
     @Autowired
     private AsyncServiceImpl asyncServiceImpl;
+
+    @Autowired
+    private QuizScheduleRepository quizScheduleRepository;
     /***
      *
      * @return
@@ -103,5 +111,40 @@ public class QuestionsController {
         Map<String, Object> map = new HashMap<>();
         map.put("message", "Success");
         return map;
+    }
+
+    @GetMapping(value = "/api/send_alert/{alert_time}/{language}")
+    public void sendAlert(@PathVariable("alert_time") String alertTime,
+                          @PathVariable("language") String language) throws Exception {
+        logger.info("Scheduler triggred alert");
+
+        java.util.Date dateNow = Calendar.getInstance().getTime();
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        String dateStr = df.format(dateNow);
+        java.sql.Date date = java.sql.Date.valueOf(dateStr);
+        QuizSchedule quizScheduled = quizScheduleRepository.getQuizOnDate(date, alertTime);
+        logger.info("Query for date :: {}", dateStr);
+        logger.info("Query for name :: {}", alertTime);
+        logger.info("quizScheduled :: {}", quizScheduled);
+
+        String[] alertTimeSplit = alertTime.split("-");
+        String message = "";
+        if (language.equalsIgnoreCase("en")) {
+            message = "\uD83D\uDC49Saarthi App Quiz Alert \uD83D\uDC48\n" +
+                    "Time: " + alertTimeSplit[0] + " pm\n" +
+                    "Subject: General Awareness";
+        } else {
+            message = "\uD83D\uDC49सारथी ऐप क्विज़ अलर्ट \uD83D\uDC48\n" +
+                    "समय: दोपहर " + alertTimeSplit[0] + " बजे\n" +
+                    "विषय: सामान्य जागरूकता";
+        }
+
+        logger.info("language :: " + language);
+        logger.info("message :: " + message);
+        logger.info("sendAlert at :: " + questionsService.getTime());
+        String[] splitChat = quizScheduled.getChatId().split("~");
+        for (String chatId : splitChat) {
+            questionsService.sendQuizAlert(chatId, quizScheduled.getToken(), message);
+        }
     }
 }
